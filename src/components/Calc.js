f9a58a8ba9b5382ca69e39f075a6221364921956
Guillaume R <guillaume.rondon@gmail.com>
2020-05-22 13:40:06 +0200

    Refactoring

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -1,14 +1,28 @@
 import React from "react";
 
+function calculateImc(imc, props) {
+  imc = (props.weight / Math.pow(props.size / 100, 2)).toFixed(2);
+  return imc;
+}
+function isUnderWeight(imc) {
+  return imc < 18.5;
+}
+
+function isOverWeight(imc) {
+  return imc >= 25;
+}
+
 const Calc = (props) => {
   var imc = 0;
   if (props.weight > 0 && props.size > 0) {
-    imc = (props.weight / Math.pow(props.size / 100, 2)).toFixed(2);
+    imc = calculateImc(imc, props);
   }
   return (
     <div>
       <span id="imc">{imc}</span>
-      <span id="alert">{imc >= 25 || imc < 18.5 ? "*" : ""}</span>
+      <span id="alert">
+        {isOverWeight(imc) || isUnderWeight(imc) ? "*" : ""}
+      </span>
     </div>
   );
 };
