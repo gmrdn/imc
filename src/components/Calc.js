ba7c0556144e46086ce73586c078ab71a46204ea
Guillaume R <guillaume.rondon@gmail.com>
2020-05-22 13:33:07 +0200

    Added alerts rules

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Calc = (props) => {
-  const imc = (props.weight / Math.pow(props.size / 100, 2)).toFixed(2);
+  var imc = 0;
+  if (props.weight > 0 && props.size > 0) {
+    imc = (props.weight / Math.pow(props.size / 100, 2)).toFixed(2);
+  }
   return (
     <div>
       <span id="imc">{imc}</span>
+      <span id="alert">{imc >= 25 || imc < 18.5 ? "*" : ""}</span>
     </div>
   );
 };
--- src/components/Calc.test.js
+++ src/components/Calc.test.js
@@ -8,5 +8,25 @@ describe("Components", () => {
       const wrapper = shallow(<Calc size={178} weight={72}></Calc>);
       expect(wrapper.find("#imc")).toHaveText("22.72");
     });
+
+    test("renders 0 when the value cannot be calculated", () => {
+      const wrapper = shallow(<Calc size={0} weight={72}></Calc>);
+      expect(wrapper.find("#imc")).toHaveText("0");
+    });
+
+    test("displays a * when the calculated IMC is equal or greater than 25", () => {
+      const wrapper = shallow(<Calc size={170} weight={80}></Calc>);
+      expect(wrapper.find("#alert")).toHaveText("*");
+    });
+
+    test("displays a * when the calculated IMC is less than 18.5", () => {
+      const wrapper = shallow(<Calc size={170} weight={50}></Calc>);
+      expect(wrapper.find("#alert")).toHaveText("*");
+    });
+
+    test("displays no * when the IMC is between 18.5 and 25", () => {
+      const wrapper = shallow(<Calc size={178} weight={72}></Calc>);
+      expect(wrapper.find("#alert")).toHaveText("");
+    });
   });
 });
